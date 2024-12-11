823b5bb6e983a8f7644d093964847150e4282cc0
Jakub Dlouhý <jakub.dlouhy@ibillboard.com>
2024-12-11 09:17:31 +0100

    Make it runnable

--- build.sh
+++ build.sh
@@ -5,18 +5,18 @@ mkdir .database
 cp ./api/config/config.yaml.dist ./api/config/config.yaml
 cp ./app/.env.dist ./app/.env
 
-docker-compose build
+docker compose build
 
-docker-compose up -d database
+docker compose up -d database
 
-docker-compose down
+docker compose down
 
-docker-compose run api npm install
+docker compose run api npm install
 
-docker-compose run app npm install
+docker compose run app npm install
 
-docker-compose run api npm run database:fresh:seeded
+docker compose run api npm run database:fresh:seeded
 
-docker-compose down
+docker compose down
 
-docker-compose up --force-recreate
+docker compose up --force-recreate
